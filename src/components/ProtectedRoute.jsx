499aa3cce25bfd23922f3274316927a8d9eef2ec
Hanna Seraphim <hannaseraphim@gmail.com>
2025-09-22 17:06:53 -0400

    Update localhost to env variable

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,10 +5,11 @@ import axios from "axios";
 const ProtectedRoute = ({children}) => {
     const isAuthenticated = localStorage.getItem('vireoAccessToken');
     const [valid, setValid] = useState(null);
+    const apiUrl = import.meta.env.VITE_API_URL
 
     useEffect(() => {
         if(!isAuthenticated) return setValid(false);
-        axios.get('http://localhost:4000/users/me', {headers: { Authorization: `Bearer ${isAuthenticated}`}})
+        axios.get(`${apiUrl}/users/me`, {headers: { Authorization: `Bearer ${isAuthenticated}`}})
         .then(res => {if (res.data && res.data._id) {setValid(true);}else{localStorage.removeItem('vireoAccessToken');setValid(false);}})
         .catch(() => {localStorage.removeItem('vireoAccessToken');setValid(false);});
     }, [])
